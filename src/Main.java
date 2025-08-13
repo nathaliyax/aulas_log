b8d915e1815863a3f7a3e0f3ab329ba79b91d804
Nathalia Lima <nathalialimas@hotmail.com>
2025-08-13 09:51:46 -0300

    Delete src directory

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Pessoa p = new Pessoa();
-        p.setNome("Maria");
-        p.setIdade(25);
-        System.out.println("Nome: " + p.getNome());
-        System.out.println("Idade: " + p.getIdade());
-    }
-}
